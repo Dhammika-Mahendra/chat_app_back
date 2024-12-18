dc412a8623022e004a5603eb7cc650cc7cb80138
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-12-18 12:03:58 +0530

    user login with JWT generation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,6 @@
 const User = require("../Models/user.model.js");
+const { generateTokenAndSetCookie } = require("../Utils/generateJWT.js");
 
-const loginUser = (req, res) => {
-    res.send("Login Page");
-};
 
 const signupUser = async (req, res) => {
 
@@ -23,17 +21,50 @@ const signupUser = async (req, res) => {
         password,
         gender
     });
-
-    await newUser.save();
+    
+    if(newUser){
+        generateTokenAndSetCookie(newUser._id, res);
+        await newUser.save();
+    }
+    
     res.status(201).json({message: "User created successfully"});
-   }catch(error){
-     res.status(500).json({ error: error.message });
-   }
+    
+  }catch(error){
+        res.status(500).json({ error: error.message });
+  }
 
 };
 
+const loginUser = async (req, res) => {
+    
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    let isPasswordCorrect = password === user.password;
+    if (!user || !isPasswordCorrect) {
+        return res.status (400).json({ error: "Invalid username or password" });
+    }
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status (200).json({
+        _id: user._id,
+        fullName: user.fullName,
+        userName: user.userName
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
 const logoutUser = (req, res) => {
-    res.send("Logout Page");
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logout successful" });
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 };
 
 module.exports = { loginUser, signupUser, logoutUser };
